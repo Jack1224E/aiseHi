cbc09a07d28ebb7ba618b851ed8e3d0be962da61
Jack1224E <139150842+Jack1224E@users.noreply.github.com>
2024-08-30 22:08:19 +0530

    aadhaar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ProfileList from './profileList';
+
+function App() {
+  return (
+    <div className="App">
+     <ProfileList/>
+    </div>
+  );
+}
+
+export default App;
